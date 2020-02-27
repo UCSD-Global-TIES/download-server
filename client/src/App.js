07adeb3c30c386d5802cfe1336d6006f1abf5070
matteuc <ymc006@ucsd.edu>
2020-02-27 14:31:14 -0800

    setup basic server with socket.io

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,35 @@
 import React, { useEffect, useState } from "react";
 import { Route, Switch } from "react-router-dom";
-import { useAuth0 } from "./react-auth0-spa";
-import PrivateRoute from "./components/PrivateRoute";
 import API from './utils/API';
 import "./App.css";
 
-import Loading from "./components/Loading";
-import Landing from "./pages/Landing/index";
-import NoMatch from "./components/NoMatch";
+import SocketContext from './socket-context'
+import * as io from 'socket.io-client'
+
+const socket = io()
+
 
 function App() {
-  // const user = {
-  //   name: "Matt Chen",
-  //   email: "matt@email.com",
-  //   picture: "https://link.com"
-  // }
-  const { loading, user, isAuthenticated } = useAuth0();
-  const [userInfo, setUserInfo] = useState();
 
   useEffect(() => {
-    updateUser();
-  }, [loading]);
+  }, []);
 
-  // Retrieve/create userInfo document after Auth0 login
-  const updateUser = () => {
-    if(isAuthenticated && user && !loading){
-      setUserInfo(user);
-    }
-  };
-
-  if (loading) {
-    return (
-      <div className="App text-center">
-        <div className="mt-5">
-          <Loading />
-        </div>
-      </div>
-    );
-  }
+  
 
   return (
+        <SocketContext.Provider value={socket}>
+
       <div className="App">
         <header>
           {/* Place navigation bar here */}
         </header>
-        <Switch>
+        {/* <Switch>
           <Route exact path="/" component={Landing} />
           <PrivateRoute exact path="/private-page" component={PrivatePage} user={userInfo}/>
           <Route component={NoMatch} />
-        </Switch>
+        </Switch> */}
       </div>
+      </SocketContext.Provider>
   );
 }
 
--- routes/api/index.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const objRoutes = require("./obj");
-
-// Object routes
-router.use("/obj", objRoutes);
-
-module.exports = router;
